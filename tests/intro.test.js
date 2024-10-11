2a5135bb7e9027dc6751b0f51afde6967aa82b54
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-10-11 12:10:17 -0600

    First tests

---
+++ tests/intro.test.js
@@ -0,0 +1,38 @@
+import { describe, test, it, expect } from 'vitest';
+import { fizzBuzz, max } from '../src/intro';
+
+describe('max', () => {
+    it('should return the first argument if it is greater', () => {
+        // AAA Example: Turn off TV
+        // Arrange: Turn on the TV 
+        const a = 2;
+        const b = 1;
+        // Act: Press the power button 
+        const result = max(a, b);
+        // Assert: Verify TV is off
+        expect(result).toBe(2)
+    })
+
+    it('should return the second argument if it is greater', () => {
+        expect(max(1, 2)).toBe(2)
+    })
+
+    it('should return the first argument if arguments are equal', () => {
+        expect(max(1, 1)).toBe(1)
+    })
+})
+
+describe('fizzBuzz', () => {
+    it('should return FizzBuzz if arg is divisible by 3 and 5', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz')
+    })
+    it('should return Fizz if arg is only divisible by 3', () => {
+        expect(fizzBuzz(3)).toBe('Fizz')
+    })
+    it('should return Buzz if arg is only divisible by 5', () => {
+        expect(fizzBuzz(5)).toBe('Buzz')
+    })
+    it('should return arg as a string if it is not divisible by 3 or 5', () => {
+        expect(fizzBuzz(4)).toBe('4')
+    })
+})
