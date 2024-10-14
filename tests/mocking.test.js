da78adaefd5db8faa5245386b15eff8aae23c487
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-10-14 09:09:29 -0600

    Interaction testing

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,10 +1,12 @@
 import { vi, it, expect, describe } from 'vitest';
-import { getPriceInCurrency, getShippingInfo } from '../src/mocking';
+import { getPriceInCurrency, getShippingInfo, renderPage } from '../src/mocking';
 import { getExchangeRate } from '../src/libs/currency';
 import { getShippingQuote } from '../src/libs/shipping';
+import { trackPageView } from '../src/libs/analytics';
 
 vi.mock('../src/libs/currency')
 vi.mock('../src/libs/shipping')
+vi.mock('../src/libs/analytics')
 
 describe('test suit', () => {
     it('test case', () => {
@@ -59,5 +61,18 @@ describe('test suit', () => {
             const quote = getShippingInfo('Mexico');
 
             expect(quote).toBe('Shipping Cost: $20 (2 Days)')
+        })
+    })
+
+    describe('renderPage', () => {
+        it('should return correct content', async () => {
+            const result = await renderPage();
+
+            expect(result).toMatch(/content/i);
+        })
+        it('should call analytics', async () => {
+            await renderPage();
+
+            expect(trackPageView).toHaveBeenCalledWith('/home');
         })
     })
