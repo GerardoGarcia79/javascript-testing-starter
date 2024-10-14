6e4b8903cb596ca063ac64b92d1159716957807a
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-10-14 08:46:36 -0600

    Mocking modules

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,4 +1,8 @@
 import { vi, it, expect, describe } from 'vitest';
+import { getPriceInCurrency } from '../src/mocking';
+import { getExchangeRate } from '../src/libs/currency';
+
+vi.mock('../src/libs/currency')
 
 describe('test suit', () => {
     it('test case', () => {
@@ -26,5 +30,16 @@ describe('test suit', () => {
             expect(sendText).toHaveBeenCalledOnce();
             expect(result).toBe('ok');
             expect(sendText).toHaveBeenCalledWith('message');
+        })
+    })
+
+
+    describe('getPriceInCurrency', () => {
+        it('should return price in target currency', () => {
+            vi.mocked(getExchangeRate).mockReturnValue(1.5);
+
+            const price = getPriceInCurrency(10, 'AUD');
+
+            expect(price).toBe(15);
         })
     })
